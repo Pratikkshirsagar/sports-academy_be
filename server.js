a880e3eedc84e525e04b296b21f39db812b94cf4
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-15 23:16:27 +0530

    basic routing testing done

--- server.js
+++ server.js
@@ -7,6 +7,34 @@ const colors = require('colors');
 dotenv.config({ path: './config/config.env' });
 const app = express();
 
+app.get('/api/v1/slotgame', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    msg: 'showing all the gameslot',
+  });
+});
+
+app.get('/api/v1/slotgame/:id', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    msg: 'showing one gameslot',
+  });
+});
+
+app.post('/api/v1/slotgame', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    msg: 'added a gameslot',
+  });
+});
+
+app.delete('/api/v1/slotgame/:id', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    msg: 'deleted one gameslot',
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
